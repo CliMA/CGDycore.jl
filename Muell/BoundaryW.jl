82edbba27c52dc579370da72eb4ff9f5c261fcff
OsKnoth <50015520+OsKnoth@users.noreply.github.com>
2023-04-14 13:37:56 +0200

    remove old stuff

--- Muell/BoundaryW.jl
+++
@@ -1,9 +0,0 @@
-function BoundaryW!(wCG,v1CG,v2CG,CG,Global,iF)
-dXdxIC = Global.Metric.dXdxIC
-@views @. wCG= -(dXdxIC[:,:,1,3,1,iF] * v1CG[:,:,1] +
-  dXdxIC[:,:,1,3,2,iF] * v2CG[:,:,1])/
-  dXdxIC[:,:,1,3,3,iF]
-end
-
-
-
