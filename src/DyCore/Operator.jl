939a6ba36f28b72a9f701516bf5bb6296759c587
OsKnoth <50015520+OsKnoth@users.noreply.github.com>
2023-06-23 14:41:22 +0200

    Update Operator.jl

--- src/DyCore/Operator.jl
+++ src/DyCore/Operator.jl
@@ -1004,50 +1004,20 @@ function DivUpwindRhoTrColumn!(FRhoTrC,uC,vC,w,RhoTrC,RhoC,Fe,dXdxI,J,
         TrRe[i,j,1,Nz],TrRe[i,j,2,Nz] = RecU3(Trm1,Tr,Tr1,JCm1,JC,JC)
       end  
     end    
+  end    
+  @inbounds for iz = 2 : Nz - 1
     @inbounds for i = 1 : OrdPoly + 1
       @inbounds for j = 1 : OrdPoly + 1
-        tempZ[i,j,1,1] =0
-        wC = (dXdxI[i,j,2,1,3,1] * uC[i,j,1] +
-          dXdxI[i,j,2,1,3,2] * vC[i,j,1] + dXdxI[i,j,2,1,3,3] * w[i,j,1+1])
-        tempZ[i,j,2,1] = -1/2 * RhoC[i,j,1] * ((wC + abs(wC)) * TrRe[i,j,2,1] +
-          (wC - abs(wC)) * TrRe[i,j,1,1+1])
+        wC = (dXdxI[i,j,2,iz,3,1] * uC[i,j,iz] + dXdxI[i,j,2,iz,3,2] * vC[i,j,iz] +
+          dXdxI[i,j,1,iz+1,3,1] * uC[i,j,iz+1] + dXdxI[i,j,1,iz+1,3,2] * vC[i,j,iz+1] + 
+          (dXdxI[i,j,2,iz,3,3] + dXdxI[i,j,1,iz+1,3,3]) * w[i,j,iz])
+        Flux = 1 / 2 * ((wC + abs(wC)) * TrRe[i,j,2,iz] +
+          (wC - abs(wC)) * TrRe[i,j,2,iz])
+        FRhoTrC[i,j,iz] -= Flux
+        FRhoTrC[i,j,iz+1] += Flux
       end
     end  
-    @inbounds for iz = 2 : Nz - 1
-      @inbounds for i = 1 : OrdPoly + 1
-        @inbounds for j = 1 : OrdPoly + 1
-          wC = (dXdxI[i,j,1,iz,3,1] * uC[i,j,iz] +
-            dXdxI[i,j,1,iz,3,2] * vC[i,j,iz] + dXdxI[i,j,1,iz,3,3] * w[i,j,iz])
-          tempZ[i,j,1,iz] = -1/2 * RhoC[i,j,iz] * ((wC + abs(wC)) * TrRe[i,j,2,iz-1] + 
-            (wC - abs(wC)) * TrRe[i,j,1,iz])
-          wC = (dXdxI[i,j,2,iz,3,1] * uC[i,j,iz] +
-            dXdxI[i,j,2,iz,3,2] * vC[i,j,iz] + dXdxI[i,j,2,iz,3,3] * w[i,j,iz+1])
-          tempZ[i,j,2,iz] = -1/2 * RhoC[i,j,iz] * ((wC + abs(wC)) * TrRe[i,j,2,iz] +
-            (wC - abs(wC)) * TrRe[i,j,1,iz+1])
-        end
-      end  
-    end    
-    @inbounds for i = 1 : OrdPoly + 1
-      @inbounds for j = 1 : OrdPoly + 1
-        wC = (dXdxI[i,j,1,Nz,3,1] * uC[i,j,Nz] +
-          dXdxI[i,j,1,Nz,3,2] * vC[i,j,Nz] + dXdxI[i,j,1,Nz,3,3] * w[i,j,Nz]) 
-        tempZ[i,j,1,Nz] = -1/2 * RhoC[i,j,Nz] * ((wC + abs(wC)) * TrRe[i,j,2,Nz-1] +
-          (wC - abs(wC)) * TrRe[i,j,1,Nz])
-        tempZ[i,j,2,Nz] = 0
-      end
-    end  
-  else
-   @. tempZ = 0   
   end    
-
-  @inbounds for iz = 1 : Nz  
-    @views @. FRhoTrC[:,:,iz] += (tempZ[:,:,2,iz] - tempZ[:,:,1,iz])
-  end    
-  @inbounds for iz = 1 : Nz - 1  
-    @views @. FluxZ = 1/2 * (tempZ[:,:,1,iz+1] - tempZ[:,:,2,iz]) 
-    @views @. FRhoTrC[:,:,iz] += FluxZ
-    @views @. FRhoTrC[:,:,iz+1] += FluxZ
-  end 
 end 
 
 function DivUpwindRhoTrColumn!(FRhoTrC,uC,vC,w,RhoTrC,RhoC,Fe,dXdxI,J,
