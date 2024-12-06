595d1ac4700fa3942f2f599de5847fd01174d357
OsKnoth <50015520+OsKnoth@users.noreply.github.com>
2024-12-06 11:38:47 +0100

    Garbage

--- src/FEMSei/VecDG.jl
+++ src/FEMSei/VecDG.jl
@@ -64,7 +64,61 @@ function VecDG0Struct{FT}(::Grids.Quad,backend,Grid) where FT<:AbstractFloat
       M,
       LUM,
         )
-  end
+end
+
+#VecDG0 Tri
+
+function VecDG0Struct{FT}(::Grids.Tri,backend,Grid) where FT<:AbstractFloat
+    Glob = KernelAbstractions.zeros(backend,Int,0,0)
+    Type = Grids.Tri()
+    DoF = 3 
+    Comp = 3
+    @polyvar x1 x2
+    phi = Array{Polynomial,2}(undef,DoF,Comp)
+    phi[1,1] = 1.0 + 0.0*x1 + 0.0*x2
+    phi[1,2] = 0.0 + 0.0*x1 + 0.0*x2
+    phi[1,3] = 0.0 + 0.0*x1 + 0.0*x2
+    phi[2,1] = 0.0 + 0.0*x1 + 0.0*x2
+    phi[2,2] = 1.0 + 0.0*x1 + 0.0*x2
+    phi[2,3] = 0.0 + 0.0*x1 + 0.0*x2
+    phi[3,1] = 0.0 + 0.0*x1 + 0.0*x2
+    phi[3,2] = 0.0 + 0.0*x1 + 0.0*x2
+    phi[3,3] = 1.0 + 0.0*x1 + 0.0*x2
+    
+    Gradphi = Array{Polynomial,3}(undef,DoF,Comp,2)
+    for i = 1 : DoF
+        for j = 1 : Comp
+            Gradphi[i,j,1] = differentiate(phi[i,j],x1)
+            Gradphi[i,j,2] = differentiate(phi[i,j],x2)
+        end
+    end
+      
+    Glob = KernelAbstractions.zeros(backend,Int,DoF,Grid.NumFaces)
+    GlobCPU = zeros(Int,DoF,Grid.NumFaces)
+    NumG = DoF * Grid.NumFaces
+    NumI = DoF * Grid.NumFaces
+    for iF = 1 : Grid.NumFaces
+        for iD = 1 : DoF
+            GlobCPU[iD,iF] = (Grid.Faces[iF].F - 1 ) * DoF + iD
+        end
+    end
+    copyto!(Glob,GlobCPU)
+    M = sparse([1],[1],[1.0])
+    LUM = lu(M)
+    return VecDG0Struct{FT,
+                    typeof(Glob)}( 
+      Glob,
+      DoF,
+      Comp,
+      phi,
+      Gradphi,                      
+      NumG,
+      NumI,
+      Type,
+      M,
+      LUM,
+        )
+end
 
 mutable struct VecDG1Struct{FT<:AbstractFloat,
                         IT2<:AbstractArray} <: VectorElement
@@ -170,4 +224,90 @@ function VecDG1Struct{FT}(::Grids.Quad,backend,Grid) where FT<:AbstractFloat
       M,
       LUM,
         )
-  end
+end
+
+#VecDG1 Tri
+
+function VecDG1Struct{FT}(::Grids.Tri,backend,Grid) where FT<:AbstractFloat
+    Glob = KernelAbstractions.zeros(backend,Int,0,0)
+    Type = Grids.Tri()
+    DoF = 9 #vorn
+    Comp = 3
+    @polyvar ksi1 ksi2
+    phi = Array{Polynomial,2}(undef,DoF,Comp)
+    phi[1,1] = 0.0*ksi1 + 0.0*ksi2 + 2.0
+    phi[1,2] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[1,3] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+
+    phi[2,1] = 0.0*ksi1 + 0.0*ksi2 + 2.0
+    phi[2,2] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[2,3] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+
+    phi[3,1] = 0.0*ksi1 + 0.0*ksi2 + 2.0
+    phi[3,2] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[3,3] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+
+    phi[4,1] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[4,2] = 0.0*ksi1 + 1.0*ksi2 - 1.0/3.0
+    phi[4,3] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+
+    phi[5,1] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[5,2] = 0.0*ksi1 + 1.0*ksi2 - 1.0/3.0
+    phi[5,3] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+
+    phi[6,1] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[6,2] = 0.0*ksi1 + 1.0*ksi2 - 1.0/3.0
+    phi[6,3] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+
+    phi[7,1] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[7,2] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[7,3] = 1.0*ksi1 + 0.0*ksi2 - 1.0/3.0
+
+    phi[8,1] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[8,2] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[8,3] = 1.0*ksi1 + 0.0*ksi2 - 1.0/3.0
+
+    phi[9,1] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[9,2] = 0.0 + 0.0*ksi1 + 0.0*ksi2
+    phi[9,3] = 1.0*ksi1 + 0.0*ksi2 - 1.0/3.0
+
+    for s = 1 : DoF
+        for t = 1 : Comp
+          phi[s,t] = subs(nu[s,t], ksi1 => (x1+1)/2, ksi2 => (x2+1)/2)
+        end
+    end
+    
+    Gradphi = Array{Polynomial,3}(undef,DoF,Comp,2)
+    for i = 1 : DoF
+        for j = 1 : Comp
+            Gradphi[i,j,1] = differentiate(phi[i,j],x1)
+            Gradphi[i,j,2] = differentiate(phi[i,j],x2)
+        end
+    end
+      
+    Glob = KernelAbstractions.zeros(backend,Int,DoF,Grid.NumFaces)
+    GlobCPU = zeros(Int,DoF,Grid.NumFaces)
+    NumG = DoF * Grid.NumFaces
+    NumI = DoF * Grid.NumFaces
+    for iF = 1 : Grid.NumFaces
+        for iD = 1 : DoF
+            GlobCPU[iD,iF] = (Grid.Faces[iF].F - 1 ) * DoF + iD
+        end
+    end
+    copyto!(Glob,GlobCPU)
+    M = sparse([1],[1],[1.0])
+    LUM = lu(M)
+    return VecDG1Struct{FT,
+                    typeof(Glob)}( 
+      Glob,
+      DoF,
+      Comp,
+      phi,
+      Gradphi,                      
+      NumG,
+      NumI,
+      Type,
+      M,
+      LUM,
+        )
+end
