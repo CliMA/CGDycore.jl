1446b4d0600e39569a8f67aba8c09a11458cc04f
Gabriele Bozzola <sbozzolator@gmail.com>
2025-02-05 10:24:41 -0800

    Add documentation

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using CGDycore
+using Documenter
+
+makedocs(;
+    modules = [
+        CGDycore,
+    ],
+    authors = "Oswald Knoth",
+    sitename = "CGDycore.jl",
+    format = Documenter.HTML(;
+        prettyurls = !isempty(get(ENV, "CI", "")),
+        collapselevel = 1,
+    ),
+    checkdocs = :exports,
+    pages = [
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(; repo = "github.com/CliMA/CGDycore.jl")
