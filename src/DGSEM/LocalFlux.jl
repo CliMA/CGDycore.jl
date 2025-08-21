cd2bc758fedcbd817e70d6ffc68ea5509840b335
Marco Artiano <artianomarco97@gmail.com>
2025-08-21 11:16:08 +0200

    fix RiemannSolverFast

--- src/DGSEM/LocalFlux.jl
+++ src/DGSEM/LocalFlux.jl
@@ -341,7 +341,6 @@ end
 
 function (::RiemannLMARSFast)(Param,Phys,RhoPos,uPos,vPos,wPos,ThPos,pPos)
   @inline function RiemannByLMARSFast!(F,VLL,VRR,AuxL,AuxR,Normal)
-
     FT = eltype(F)
     cS = Param.cS
     pLL = AuxL[pPos]
@@ -354,6 +353,7 @@ function (::RiemannLMARSFast)(Param,Phys,RhoPos,uPos,vPos,wPos,ThPos,pPos)
     F[uPos] = Normal[1] * pM
     F[vPos] = Normal[2] * pM
     F[wPos] = Normal[3] * pM
+    @show "sono qui"		
     if vM > FT(0)
       F[RhoPos] = vM * VLL[RhoPos]
       F[RhoThPos] = F[RhoPos] * VLL[RhoThPos]
@@ -363,6 +363,34 @@ function (::RiemannLMARSFast)(Param,Phys,RhoPos,uPos,vPos,wPos,ThPos,pPos)
     end
   end
   return RiemannByLMARSFast!
+end
+
+function (::RiemannLMARSSlow)(Param,Phys,RhoPos,uPos,vPos,wPos,ThPos,pPos)
+  @inline function RiemannByLMARSNonLin!(F,VLL,VRR,AuxL,AuxR,Normal)
+
+    FT = eltype(F)
+    cS = Param.cS
+    pLL = AuxL[pPos]
+    pRR = AuxR[pPos]
+    RhoM = FT(0.5) * (VLL[RhoPos] + VRR[RhoPos])
+    vLL = (VLL[uPos] * Normal[1] + VLL[vPos] * Normal[2] + VLL[wPos] * Normal[3]) / VLL[RhoPos]
+    vRR = (VRR[uPos] * Normal[1] + VRR[vPos] * Normal[2] + VRR[wPos] * Normal[3]) / VRR[RhoPos]
+    vM = FT(0.5) * (vRR + vLL) - FT(1.0) /(FT(2.0) * cS) * (pRR - pLL) / RhoM
+    if vM > FT(0)
+      F[RhoPos] = 0.0
+      F[uPos] = vM * VLL[uPos]
+      F[vPos] = vM * VLL[vPos]
+      F[wPos] = vM * VLL[wPos]
+      F[ThPos] = 0.0
+    else
+      F[RhoPos] = 0.0
+      F[uPos] = vM * VRR[uPos]
+      F[vPos] = vM * VRR[vPos]
+      F[wPos] = vM * VRR[wPos]
+      F[ThPos] = 0.0
+    end
+  end
+  return RiemannByLMARSNonLin!
 end
 
 function (::RiemannLMARSLin)(Param,Phys,RhoPos,uPos,vPos,wPos,RhoThPos,dpdRhoThPos,ThPos)
